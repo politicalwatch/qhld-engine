a3c71191ff2a12f8d86d607c6c467880df1036d8
David Hernandez <davidhernandezruiz@gmail.com>
2021-07-23 18:34:00 +0200

    Adds basic tagging using the new data model. Still has to fix the clean up for single occurences.

--- tagger/tag_initiatives.py
+++ tagger/tag_initiatives.py
@@ -3,9 +3,11 @@ import pickle
 import codecs
 
 import tipi_tasks
-from tipi_data.models.topic import Topic
-from tipi_data.models.initiative import Initiative, Tag
-from tipi_data.models.alert import InitiativeAlert, create_alert
+from tipi_data.repositories.tags import Tags
+from tipi_data.repositories.initiatives import Initiatives
+from tipi_data.repositories.alerts import Alerts
+from tipi_data.models.initiative import Tag
+from tipi_data.models.alert import create_alert
 
 from logger import get_logger
 from alerts.settings import USE_ALERTS
@@ -23,18 +25,6 @@ class TagInitiatives:
         return self.__same_field(tag1, tag2, 'topic') \
                 and self.__same_field(tag1, tag2, 'subtopic') \
                 and self.__same_field(tag1, tag2, 'tag')
-
-    def __delete_topics_with_one_tag_ocurrence(self, result):
-        topics_counter = dict()
-        for tag in result['tags']:
-            if tag['topic'] in topics_counter.keys():
-                topics_counter[tag['topic']] += tag['times']
-            else:
-                topics_counter[tag['topic']] = tag['times']
-        for key in topics_counter.keys():
-            if topics_counter[key] == 1:
-                result['tags'] = list(filter(lambda x: x['topic'] != key, result['tags']))
-        result['topics'] = sorted(list(set([tag['topic'] for tag in result['tags']])))
 
     def __merge_results(self, title_result, body_result):
         DEFAULT_RESULT = {
@@ -60,17 +50,13 @@ class TagInitiatives:
                 'tags': merged_tags
                 }
 
-    def __get_untagged_query(self):
-        return {
-                '$or': [
-                    {'tagged': False},
-                    {'tagged': {'$exists': False}},
-                    ]
-                }
 
     def tag_initiatives(self, initiatives, tags, merge=False, send_alerts=True):
         total = len(initiatives)
         for index, initiative in enumerate(initiatives):
+            if not merge:
+                initiative.untag()
+
             try:
                 log.info(f"Tagging initiative {index+1} of {total}")
                 tipi_tasks.init()
@@ -87,55 +73,43 @@ class TagInitiatives:
                         continue
                     body_result = body_result['result']
                     result = self.__merge_results(title_result, body_result)
-                new_tags = list(map(
-                    lambda x: Tag(
-                        topic=x['topic'],
-                        subtopic=x['subtopic'],
-                            tag=x['tag'],
-                        times=x['times']
-                        ), result['tags']))
 
-                if merge:
-                    topics = list(set(initiative['topics'] + result['topics']))
-                    new_tags = self.merge_tags(initiative['tags'], new_tags)
-                else:
-                    topics = result['topics']
+                for tag in result['tags']:
+                    initiative.add_tag(tag['knowledgebase'], tag['topic'], tag['subtopic'], tag['tag'], tag['times'])
 
-                initiative['tags'] = new_tags
-                initiative['topics'] = topics
-                self.__delete_topics_with_one_tag_ocurrence(initiative)
-                initiative['tagged'] = True
+                # TODO continue here. This is not working.
+                initiative.remove_single_occurences()
                 initiative.save()
-                if len(result['topics']) > 0 and USE_ALERTS and send_alerts:
+                if initiative.has_tags() and USE_ALERTS and send_alerts:
                     create_alert(initiative)
             except Exception as e:
                 log.error(f"Error tagging {initiative['id']}: {e}")
 
     def run(self):
-        InitiativeAlert.objects().delete()
-        tags = codecs.encode(pickle.dumps(Topic.get_tags()), "base64").decode()
-        initiatives = list(Initiative.all(__raw__=self.__get_untagged_query()))
+        Alerts.get_all().delete()
+        tags = codecs.encode(pickle.dumps(Tags.get_all()), "base64").decode()
+        initiatives = list(Initiatives.get_all_untagged())
         self.tag_initiatives(initiatives, tags)
 
     def tag_kb(self, kb):
-        tags = Topic.get_tags_by_kb(kb)
+        tags = Tags.by_kb(kb)
         tags = codecs.encode(pickle.dumps(tags), "base64").decode()
-        initiatives = list(Initiative.all())
+        initiatives = list(Initiatives.get_all())
         self.tag_initiatives(initiatives, tags, True, True)
 
     def new_tag(self, tag):
-        tags = Topic.get_filtered_tags('tag', tag)
+        tags = Tags.by_name(tag)
         tags = codecs.encode(pickle.dumps(tags), "base64").decode()
-        initiatives = list(Initiative.all())
+        initiatives = list(Initiatives.get_all())
         self.tag_initiatives(initiatives, tags, True, False)
 
     def new_topic(self, topic):
-        tags = codecs.encode(pickle.dumps(Topic.get_filtered_tags_by_topic(topic)), "base64").decode()
-        initiatives = list(Initiative.all())
+        tags = codecs.encode(pickle.dumps(Tags.by_topic(topic)), "base64").decode()
+        initiatives = list(Initiatives.get_all())
         self.tag_initiatives(initiatives, tags, True, False)
 
     def rename(self, old_tag, new_tag):
-        initiatives = Initiative.all(tags__tag=old_tag)
+        initiatives = Initiatives.by_tag(old_tag)
         for initiative in initiatives:
             for tag in initiative['tags']:
                 if tag.tag == old_tag:
