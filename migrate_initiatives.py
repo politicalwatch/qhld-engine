732bdc84b44c48a8381148f9afc03f26f0ff2df4
David Hernandez <davidhernandezruiz@gmail.com>
2021-09-17 15:49:56 +0200

    Adds script to merge initiatives from tipi and p2030.

---
+++ migrate_initiatives.py
@@ -0,0 +1,75 @@
+from tipi_data import db
+from tipi_data.models.initiative import Initiative
+
+
+class Tipi(db.DynamicDocument):
+    meta = {'collection': 'initiatives-tipi'}
+
+class P2030(db.DynamicDocument):
+    meta = {'collection': 'initiatives-p2030'}
+
+collections = [
+    {
+        'collection': P2030,
+        'knowledgebase': 'parlamento2030'
+    },
+    {
+        'collection': Tipi,
+        'knowledgebase': 'tipiciudadano'
+    },
+]
+
+fields = [
+    'id',
+    'title',
+    'initiative_type',
+    'initiative_type_alt',
+    'author_deputies',
+    'author_others',
+    'author_parliamentarygroups',
+    'place',
+    'created',
+    'updated',
+    'history',
+    'status',
+    'url',
+    'content',
+    'extra',
+]
+
+def merge_initiatives():
+    for collection in collections:
+        db_collection = collection['collection']
+        kb = collection['knowledgebase']
+
+        initiatives = db_collection.objects()
+
+        for new_initiative in initiatives:
+            initiative = get_initiative(new_initiative['reference'])
+
+            for field in fields:
+                set_field(field, new_initiative, initiative)
+                add_tags(kb, new_initiative, initiative)
+
+            initiative.save()
+
+def get_initiative(reference):
+    try:
+        return Initiative.all.get(reference=reference)
+    except Exception:
+        return Initiative(reference=reference)
+
+def set_field(field, origin, destination):
+    exist_on_origin = field in origin
+    empty_on_destination = field not in destination or destination[field] == None
+    if exist_on_origin and empty_on_destination:
+        destination[field] = origin[field]
+
+def add_tags(kb, origin, destination):
+    if 'tags' not in origin:
+        return
+
+    for tag in origin['tags']:
+        destination.add_tag(kb, True, tag['topic'], tag['subtopic'], tag['tag'], tag['times'])
+
+merge_initiatives()
