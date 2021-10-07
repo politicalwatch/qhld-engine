9b779db60688db505ab5cd6cae0be2e7201446ee
David Hernandez <davidhernandezruiz@gmail.com>
2021-10-07 16:21:23 +0200

    adds script to merge the alerts.

--- migrate_initiatives.py
+++ migrate_initiatives.py
@@ -1,5 +1,9 @@
+import json
+
 from tipi_data import db
 from tipi_data.models.initiative import Initiative
+from tipi_data.models.alert import Alert, Search
+from tipi_data.utils import generate_id
 
 
 class Tipi(db.DynamicDocument):
@@ -8,13 +12,21 @@ class Tipi(db.DynamicDocument):
 class P2030(db.DynamicDocument):
     meta = {'collection': 'initiatives-p2030'}
 
-collections = [
+class TipiAlerts(db.DynamicDocument):
+    meta = {'collection': 'alerts-tipi'}
+
+class P2030Alerts(db.DynamicDocument):
+    meta = {'collection': 'alerts-p2030'}
+
+sources = [
     {
-        'collection': P2030,
+        'initiatives': P2030,
+        'alerts': P2030Alerts,
         'knowledgebase': 'parlamento2030'
     },
     {
-        'collection': Tipi,
+        'initiatives': Tipi,
+        'alerts': TipiAlerts,
         'knowledgebase': 'tipiciudadano'
     },
 ]
@@ -39,11 +51,11 @@ fields = [
 ]
 
 def merge_initiatives():
-    for collection in collections:
-        db_collection = collection['collection']
-        kb = collection['knowledgebase']
+    for source in sources:
+        collection = source['initiatives']
+        kb = source['knowledgebase']
 
-        initiatives = db_collection.objects()
+        initiatives = collection.objects()
 
         for new_initiative in initiatives:
             initiative = get_initiative(new_initiative['id'])
@@ -98,6 +110,73 @@ def merge_and_delete_answers():
         question.save()
         answer.delete()
 
+search_fields = [
+        'hash',
+        'search',
+        'dbsearch',
+        'created',
+        'validated',
+        'validation_email_sent',
+        'validation_email_sent_date',
+        ]
 
-merge_initiatives()
-merge_and_delete_answers()
+def merge_alerts():
+    for source in sources:
+        collection = source['alerts']
+        kb = source['knowledgebase']
+        print('Merging alerts from knowledgebase ' + kb)
+
+        alerts = collection.objects()
+        count = alerts.count()
+        counter = 0
+        for new_alert in alerts:
+            counter += 1
+            print(str(counter) + ' out of ' + str(count))
+
+            alert = get_alert(new_alert['email'])
+
+            for alert_search in new_alert['searches']:
+                search = copy_search(alert_search, kb)
+
+                if 'searches' not in alert:
+                    alert['searches'] = []
+
+                alert['searches'].append(search)
+            alert.save()
+
+def copy_search(source, kb):
+    search = json.loads(source['search'])
+    search['knowledgebase'] = kb
+    return Search(
+            hash=source['hash'],
+            search=json.dumps(search),
+            dbsearch=source['dbsearch'],
+            created=source['created'],
+            validated=source['validated'],
+            validation_email_sent=source['validation_email_sent'],
+            validation_email_sent_date=source['validation_email_sent_date']
+            )
+
+loaded_alerts = {}
+def get_alert(email):
+    email = email.replace('gmail-com', 'gmail.com')
+
+    if email in loaded_alerts:
+        return loaded_alerts[email]
+
+    try:
+        alert = Alert.objects().get(email=email)
+    except Exception:
+        alert = Alert(
+            id=generate_id(email),
+            email=email
+        )
+    loaded_alerts[email] = alert
+    return alert
+
+def execute_merge():
+    merge_initiatives()
+    merge_and_delete_answers()
+    merge_alerts()
+
+execute_merge()
